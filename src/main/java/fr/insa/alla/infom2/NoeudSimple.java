9cf136df039bea3f5acab9ff469d6bcf0bc09694
Farouk Mahfoudh <farouk@mahfoudh.net>
2022-03-18 17:07:36 +0100

    Noeud devient un abstract et on a ajoute une classe NoeudSimple

---
+++ src/main/java/fr/insa/alla/infom2/NoeudSimple.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.alla.infom2;
+
+import fr.insa.nomEtud.nomProj.Noeud;
+
+/**
+ *
+ * @author Farouk
+ */
+public class NoeudSimple extends Noeud {
+    
+}
