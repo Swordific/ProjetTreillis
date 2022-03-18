59973df6bb520c959e78f380361ee5e31dc839c2
antoi <antoi@172.29.40.105>
2022-03-18 17:31:31 +0100

    Debut appui simple

---
+++ src/main/java/fr/insa/alla/infom2/NoeudAppuiSimple.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.alla.infom2;
+
+/**
+ *
+ * @author antoi
+ */
+public class NoeudAppuiSimple {
+    
+    @Override
+    public String toString() {
+        String output = new String();
+        return output;
+    }
+    
+    
+    
+}
