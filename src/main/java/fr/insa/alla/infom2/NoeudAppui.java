6852a8cd2554e820e15c5e8ba56b0fddcc25cd9a
hazem2is <hazemalhamed202@gmail.com>
2022-03-18 17:20:07 +0100

    class NoeudAppui creation

---
+++ src/main/java/fr/insa/alla/infom2/NoeudAppui.java
@@ -0,0 +1,14 @@
+package fr.insa.alla.infom2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hazem
+ */
+public class NoeudAppui extends Noeud {
+    
+}
