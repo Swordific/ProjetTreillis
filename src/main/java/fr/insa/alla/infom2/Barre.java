b60a6cc6797e5f323a04d06a5dbb558a800a468c
antoi <antoi@172.29.40.105>
2022-03-24 09:39:05 +0100

    continue appui simple et classe barre

---
+++ src/main/java/fr/insa/alla/infom2/Barre.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.alla.infom2;
+
+/**
+ *
+ * @author antoi
+ */
+public class Barre {
+    
+    private int id;
+    private int NoeudDepart;
+    private int NoeudArrivee;
+    
+    
+    
+    
+}
