fc02c1cf15ada74abc856903460d1d9ed7774610
Swordific <alla38@live.fr>
2022-03-30 15:55:44 +0200

    Nouveau dossier entree noeud améliorée

---
+++ src/main/java/fr/insa/alla/infom2/ProjetTreillis/NoeudSimple.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.alla.infom2;
+
+/**
+ *
+ * @author Farouk
+ */
+public class NoeudSimple extends Noeud {
+    
+    private int id;
+    private double px, py;
+    private Vecteur2D f;
+
+    public NoeudSimple(int id, double px, double py, Vecteur2D f) {
+        super(id, px, py, f);
+    }
+    
+    @Override
+    public String toString() {
+        String output = new String();
+        return output;
+    }
+    
+    /**
+     * @return the id
+     */
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the px
+     */
+    @Override
+    public double getPx() {
+        return px;
+    }
+
+    /**
+     * @param px the px to set
+     */
+    @Override
+    public void setPx(double px) {
+        this.px = px;
+    }
+
+    /**
+     * @return the py
+     */
+    @Override
+    public double getPy() {
+        return py;
+    }
+
+    /**
+     * @param py the py to set
+     */
+    @Override
+    public void setPy(double py) {
+        this.py = py;
+    }
+
+    /**
+     * @return the f
+     */
+    @Override
+    public Vecteur2D getF() {
+        return f;
+    }
+
+    /**
+     * @param f the f to set
+     */
+    @Override
+    public void setF(Vecteur2D f) {
+        this.f = f;
+    }
+}
