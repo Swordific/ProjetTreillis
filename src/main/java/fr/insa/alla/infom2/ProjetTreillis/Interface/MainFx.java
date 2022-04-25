32792e34fb64db91ff66c080e9965b4da396dc0c
Swordific <alla38@live.fr>
2022-04-25 15:35:57 +0200

    implentation de javafx dans le projet

---
+++ src/main/java/fr/insa/alla/infom2/ProjetTreillis/Interface/MainFx.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.alla.infom2.ProjetTreillis.Interface;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Asus
+ */
+public class MainFx extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
