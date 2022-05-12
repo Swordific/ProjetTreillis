374f71366a5d5762f4bb9af03415ebf7140658cc
AntoineL07 <antoinelietaert@gmail.com>
2022-05-12 08:16:10 +0100

    Creation classe terrain

--- src/main/java/fr/insa/alla/infom2/ProjetTreillis/Terrain.java
+++ src/main/java/fr/insa/alla/infom2/ProjetTreillis/Terrain.java
@@ -9,5 +9,67 @@ package fr.insa.alla.infom2.ProjetTreillis;
  * @author Asus
  */
 public class Terrain {
+    
+    private double Xmin, Xmax, Ymin, Ymax;
+
+    /**
+     * @return the Xmin
+     */
+    public double getXmin() {
+        return Xmin;
+    }
+
+    /**
+     * @param Xmin the Xmin to set
+     */
+    public void setXmin(double Xmin) {
+        this.Xmin = Xmin;
+    }
+
+    /**
+     * @return the Xmax
+     */
+    public double getXmax() {
+        return Xmax;
+    }
+
+    /**
+     * @param Xmax the Xmax to set
+     */
+    public void setXmax(double Xmax) {
+        this.Xmax = Xmax;
+    }
+
+    /**
+     * @return the Ymin
+     */
+    public double getYmin() {
+        return Ymin;
+    }
+
+    /**
+     * @param Ymin the Ymin to set
+     */
+    public void setYmin(double Ymin) {
+        this.Ymin = Ymin;
+    }
+
+    /**
+     * @return the Ymax
+     */
+    public double getYmax() {
+        return Ymax;
+    }
+
+    /**
+     * @param Ymax the Ymax to set
+     */
+    public void setYmax(double Ymax) {
+        this.Ymax = Ymax;
+    }
+    
+    
+    
+    
 
 }
