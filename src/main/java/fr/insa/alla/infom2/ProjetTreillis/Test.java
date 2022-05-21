a6baafb42af5d1f93a46f9475f0eee817d9ad517
hazem2is <hazemalhamed202@gmail.com>
2022-05-21 22:48:14 +0200

    chepa ca beug

--- src/main/java/fr/insa/alla/infom2/ProjetTreillis/Test.java
+++ src/main/java/fr/insa/alla/infom2/ProjetTreillis/Test.java
@@ -20,8 +20,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerException;
-import java.lang.Math.* ;
-
+import java.lang.Math.*;
 
 /**
  *
@@ -40,33 +39,29 @@ public class Test {
         //Numeroteur num = new Numeroteur();
         //num.addAll(667, "dfnoifdn", 994.01);
         //System.out.println(num.getIndex(667));
-
-
-
-        Vecteur2D f = new Vecteur2D(0, 1000);
-        Noeud n1 = new NoeudAppuiSimple(1, 0, 0, f);
-        Noeud n2 = new NoeudAppuiSimple(2, 2, 0, f);
-        Noeud n3 = new NoeudSimple(3, 1, 1, f);
-                
-        Barre b1 = new Barre(n1, n2);
-        Barre b2 = new Barre(n2, n3);
-        Barre b3 = new Barre(n3, n1);
-        
+//
+//        Vecteur2D f = new Vecteur2D(0, 1000);
+//        Noeud n1 = new NoeudAppuiSimple(1, 0, 0, f);
+//        Noeud n2 = new NoeudAppuiSimple(2, 2, 0, f);
+//        Noeud n3 = new NoeudSimple(3, 1, 1, f);
+//                
+//        Barre b1 = new Barre(n1, n2);
+//        Barre b2 = new Barre(n2, n3);
+//        Barre b3 = new Barre(n3, n1);
+//        
        
 
         
         Treillis t = new Treillis();
-        t.ajouteNoeud(n2);
-        t.ajouteNoeud(n1);
-        t.ajouteNoeud(n3);
-        t.ajouteBarre(b1);
-        t.ajouteBarre(b2);
-        t.ajouteBarre(b3);
+//        t.ajouteNoeud(n2);
+//        t.ajouteNoeud(n1);
+//        t.ajouteNoeud(n3);
+//        t.ajouteBarre(b1);
+//        t.ajouteBarre(b2);
+//        t.ajouteBarre(b3);
         menuTexte(t);
     
 
-
-        
 
       
                 
@@ -75,4 +70,3 @@ public class Test {
         
     }
 }
-
